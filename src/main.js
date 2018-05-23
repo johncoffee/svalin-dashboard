1bac087ee522346a4e21e0ed6ba6ad7a0e66450d
johncoffee <github2017@ragelse.dk>
2018-05-23 15:09:39 +0200

    second

---
+++ src/main.js
@@ -0,0 +1,43 @@
+
+export function init(window) {
+
+  const delay = parseInt(sessionStorage.refresh, 10) * 1000 || 60 * 1000
+  const render = () => {
+    console.debug('reload..')
+    window.location.reload()
+  }
+  setInterval(() => render(), delay)
+
+  parseUrl()
+}
+
+export function parseUrl() {
+
+  let q = location.search
+  q = q.substr( q.indexOf("q=")+2 )
+
+  const frameIds = q.split(",")
+
+  const el = document.querySelector('.iframe-list')
+  let html = frameIds
+    .filter(id => !!id)
+    .map(id => {
+      return `
+        <div class="cell auto">
+            <iframe class="iframe-graph" frameborder="0" scrolling="no" src="https://plot.ly/~piredtu/${id}.embed"></iframe>
+        </div>            
+      `
+    })
+    .join('')
+
+  if (!html) {
+    html = `
+      <h3>
+        Add some IDs to the url, fx <code>http://localhost:8080/?q=35,36</code>
+      </h3>
+    `
+  }
+
+  el.innerHTML = html
+}
+
